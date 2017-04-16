670613294e843bd1f84fc407b32852c3afd9116e
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2017-04-16 19:39:17 +0900

    Add handlbers view engine

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
+const hbs = require('express-hbs')
 
 require('dotenv').config()
 
@@ -9,8 +10,20 @@ require('dotenv').config()
 const staticPath = path.join(__dirname, 'public')
 app.use('/static', express.static(staticPath))
 
-// GET /index
+// set view engine
+app.engine('hbs', hbs.express4({
+  partialsDir: path.join(__dirname, 'views', 'partials')
+}))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+// GET /
 app.get('/', (req, res) => {
+  res.render('index', {env: process.env.NODE_ENV})
+})
+
+// GET /hello
+app.get('/hello', (req, res) => {
   const nodeEnv = process.env.NODE_ENV
   res.send(`Hello, World! (${nodeEnv})`)
 })
