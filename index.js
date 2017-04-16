ee1c427f634966961946102f599b6b87e58e518c
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2017-04-16 16:48:12 +0900

    add .editorconfig

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 3000;
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.send('Hello, World!')
