c277bef831a49c34a6d6cde0f2e116b61ad19055
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2017-04-16 16:19:02 +0900

    change port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('Hello, World!')
 })
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
