12da8ac97e9deb871138be28e2fd8e964e348711
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2017-04-16 19:23:39 +0900

    Add static files

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
 
 require('dotenv').config()
 
+// static files
+const staticPath = path.join(__dirname, 'public')
+app.use('/static', express.static(staticPath))
+
+// GET /index
 app.get('/', (req, res) => {
   const nodeEnv = process.env.NODE_ENV
   res.send(`Hello, World! (${nodeEnv})`)
