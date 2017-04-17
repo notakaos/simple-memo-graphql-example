755ab2bc52f81beab503418a22616bcf39ae5407
Nobutaka OSHIRO <noshiro@stdout.me>
2017-04-18 03:04:12 +0900

    Add vue-client sources

---
+++ client/src/components/Memos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Memos</h1>
+    <ul>
+      <li v-for="memo in memos">{{ memo.id }}: {{ memo.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Memos',
+  data () {
+    return {
+      memos: [
+        {id: 1, title: 'memo 1'},
+        {id: 2, title: 'memo 2'},
+        {id: 3, title: 'memo 3'}
+      ]
+    }
+  }
+}
+</script>
