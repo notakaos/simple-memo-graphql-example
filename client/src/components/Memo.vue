ef8aad764f8f518520b3ffbcbe1f8370d4f42dea
Nobutaka OSHIRO <noshiro@stdout.me>
2017-04-22 13:25:37 +0900

    Add deleteMemo button

--- client/src/components/Memo.vue
+++ client/src/components/Memo.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="memo-item">
     <div>{{ memo.id }}: {{ memo.title }}</div>
+    <a href="#" @click="deleteMemo" class="remove-button">
+       &times;
+    </a>
     <hr />
     <textarea class="textarea" v-model="memo.content"></textarea>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
 export default {
   name: 'Memo',
   props: {
     'memo': Object
+  },
+  methods: {
+    deleteMemo () {
+      this.$apollo.mutate({
+        mutation: gql`mutation ($id: Int!) {
+          deleteMemo (id: $id)
+        }`,
+        variables: {
+          id: this.memo.id
+        }
+      })
+      .then((data) => {
+        console.log(data)
+        console.log('deleted!')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    }
   }
 }
 </script>
